1b7e0dc8fb54c4ae42f136bbcc9a5bbc504c64c1
Minvrl <m.novruzluu@gmail.com>
2024-04-05 04:27:02 +0400

    Add project files.

---
+++ WebConsole/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebConsole.Entities;
+
+namespace WebConsole
+{
+    internal class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MIMI\\SQLEXPRESS;Database=Store;Trusted_connection=true");
+        }
+    }
+}
---
+++ WebConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebConsole;
+using WebConsole.Entities;
+
+
+AppDbContext appDbContext = new AppDbContext();
+
+var appBuilder = WebApplication.CreateBuilder();
+
+
+
+appBuilder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+
+var app = appBuilder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/", () =>
+{
+    return "Hello, World!";
+});
+
+app.MapGet("/products", async () =>
+{
+    var products = await appDbContext.Products.ToListAsync();
+    return products;
+});
+
+
+app.MapGet("/products/{id}", (int id) =>
+{
+    var data = appDbContext.Products.Find(id);
+
+    if (data == null) return Results.NotFound("Not found!");
+
+    return Results.Ok(data);
+});
+
+app.MapPost("/products", (Product product) =>
+{
+    appDbContext.Add(product);
+    appDbContext.SaveChanges();
+
+    return Results.Ok();
+});
+
+
+app.Run();
